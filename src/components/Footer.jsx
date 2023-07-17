d49419b009cfa68ff8f094577457a7d766e8d652
nnrwp <rawipa_s@cmu.ac.th>
2023-07-17 16:53:07 +0700

    completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export const Footer = (props) => {
+  return (
+    <div className="mt-3">
+      <p className="text-secondary text-center">
+        copyright © {props.year} {props.fullName} {props.studentId}
+      </p>
+    </div>
+  );
+};
